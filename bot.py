d1600c244aab207cb305b8cc1abe28779debceaa
greenglobe-ai <ductientoancau4lb@gmail.com>
2025-12-04 20:59:45 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import telebot
+
+# Thay token bot của bạn vào đây
+BOT_TOKEN = "8188908206:AAHzTJ21bzkDgkXXo0tfUWauGSHQXU0tZ3M"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    bot.reply_to(message, "Chào mừng bạn đến với hệ thống bán hàng tự động Green Globe!")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Bạn vừa nhắn: {message.text}")
+
+print("Bot đang chạy...")
+bot.infinity_polling()
